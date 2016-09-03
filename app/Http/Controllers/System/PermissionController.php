689cae5be48acc8ba1e03dfb579967751c7ce1cd
zjien <694396727@qq.com>
2016-09-03 17:41:05 +0800

    [deploy] 自动部署测试

---
+++ app/Http/Controllers/System/PermissionController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace tecai\Http\Controllers\System;
+
+use Illuminate\Http\Request;
+
+use tecai\Http\Requests;
+use tecai\Http\Controllers\Controller;
+use tecai\Repositories\Interfaces\System\PermissionRepository;
+
+class PermissionController extends Controller
+{
+    protected $repository;
+
+    public function __construct(PermissionRepository $permissionRepository) {//注入仓库接口
+        $this->repository = $permissionRepository;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $this->repository->getFieldsSearchable();
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
