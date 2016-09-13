bfd987d9612c48aac294d070dc198c6a3e0d6b78
zjien <694396727@qq.com>
2016-09-13 23:55:29 +0800

    添加System和User的仓库接口与实现的绑定

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -23,8 +23,12 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        //System
         $this->app->singleton('tecai\Repositories\Interfaces\System\PermissionRepository', 'tecai\Repositories\System\PermissionRepositoryEloquent');
         $this->app->singleton('tecai\Repositories\Interfaces\System\RoleRepository', 'tecai\Repositories\System\RoleRepositoryEloquent');
+        $this->app->singleton('tecai\Repositories\Interfaces\System\AccountRepository', 'tecai\Repositories\System\AccountRepositoryEloquent');
         $this->app->singleton('tecai\Repositories\Interfaces\System\AdminRepository', 'tecai\Repositories\System\AdminRepositoryEloquent');
+        //User
+        $this->app->singleton('tecai\Repositories\Interfaces\User\UserRepository', 'tecai\Repositories\User\UserRepositoryEloquent');
     }
 }
