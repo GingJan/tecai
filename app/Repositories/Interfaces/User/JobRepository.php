621a5964486c205780d1ff6a520bb9104f3cb1d4
zjien <694396727@qq.com>
2016-09-20 18:18:21 +0800

    [deploy] 部署job仓库

---
+++ app/Repositories/Interfaces/User/JobRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace tecai\Repositories\Interfaces\User;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+use tecai\Repositories\Interfaces\CommonRepository;
+
+/**
+ * Interface JobRepository
+ * @package namespace tecai\Repositories\Interfaces\User;
+ */
+interface JobRepository extends RepositoryInterface, CommonRepository
+{
+    //
+}
