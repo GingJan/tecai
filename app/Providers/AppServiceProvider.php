864a01f675000b1acdfd5826d4b50c8624fc72c9
zjien <694396727@qq.com>
2016-10-06 23:33:03 +0800

    作修改

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,14 @@
 
 namespace tecai\Providers;
 
+use Dingo\Api\Exception\ResourceException;
+use Dingo\Api\Http\Response;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
+use Prettus\Validator\Exceptions\ValidatorException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,11 +26,22 @@ class AppServiceProvider extends ServiceProvider
 //            $handler->render(app(\Illuminate\Http\Request::class), $e);
 //        });
         \DingoApi::error(function(\Exception $e) {
-            if( $e instanceof ModelNotFoundException) {
-                //or throw new NotFoundHttpException;
+//            var_dump($e instanceof ModelNotFoundException);
+            if( $e instanceof \ErrorException) {
+//                //log;
+
+//                \DingoApi::response()->errorInternal($e->getMessage());
+            }
+            else if( $e instanceof ModelNotFoundException ) {
+//                throw new ResourceException($e->getMessage(), null, $e); //422
                 \DingoApi::response()->errorNotFound();
-            } else {
-                throw $e;
+//                return 'f';
+            }
+//            else if ( $e instanceof HttpException) {
+//                throw $e;
+//            }
+            else if ( $e instanceof ValidatorException) {
+                \DingoApi::response()->errorBadRequest($e->getMessageBag());
             }
         });
     }
@@ -39,7 +54,8 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->register(RepositoryServiceProvider::class);//项目内自定义的SP
-
+//        $this->app->register(TransformerServiceProvider::class);
+//        $this->app->register();
         //第三方包的SP，也可以在config/app下配置，但如果在config/app.php里配置，无论什么环境下都会载入该SP，虽然APP_DEBUG=false可以保证不会在正式环境上执行，但是依然会影响Laravel的启动速度和时间，也浪费内存，因为毕竟该SP会载入
 //        if ( 'production' !== $this->app->environment() ) {//当为生产环境时才注册该服务提供者
 //            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
