a07bc861279df11eb91c978ad8f92af5d230d3f0
zjien <694396727@qq.com>
2016-10-23 11:05:39 +0800

    added CorporationTableSeeder

---
+++ database/seeds/CorporationTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CorporationTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(\tecai\Models\Organization\Corporation::class, 50)->create();
+    }
+}
