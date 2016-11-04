d5b2a8d0728e66f0538bef9fef1c7257bcee5e60
zjien <694396727@qq.com>
2016-11-04 11:29:02 +0800

    添加自定义权限检查中间件

---
+++ app/Http/Middleware/PermCheck.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace tecai\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use tecai\Models\System\Account;
+use Tymon\JWTAuth\JWTAuth;
+use Tymon\JWTAuth\Token;
+
+class PermCheck
+{
+    /**
+     * @var JWTAuth
+     */
+    protected $auth;
+
+    public function __construct(JWTAuth $auth) {
+        $this->auth = $auth;
+    }
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $token = $this->auth->setRequest($request)->getToken();
+        $payload = $this->auth->manager()->decode(new Token($token));
+
+        $uri = $this->getResourceUri($request);
+        $method = $request->getMethod();
+        $account_id = $payload->get('id');
+
+        if( ! Gate::forUser(Account::findOrFail($account_id))->check($uri, $method) ) {
+            throw new AccessDeniedHttpException('无权限访问');
+        }
+
+        return $next($request);
+    }
+
+    /**
+     * 获取资源的uri
+     * @param \Illuminate\Http\Request $request
+     * @return string $resourceUri
+     */
+    protected function getResourceUri($request) {
+        $uri = $request->getPathInfo();
+        $uri = trim($uri, '/');
+
+        $segment = explode('/', $uri);
+
+        if(count($segment) % 2 === 1) {//如果是奇数个，则是访问列表
+            $resource = end($segment);
+        } else {//否则就是访问指定某个资源
+            end($segment);//id
+            $resource = prev($segment) . '/{id}';
+        }
+
+        return '/' . $resource;
+    }
+
+///bbb /前无东西，单/
+///bbb/3 /前有东西，双/
+///aaa/1/bbb/2 /前有东西，双，
+///aaa/2/bbb /前有东西，单/
+}
