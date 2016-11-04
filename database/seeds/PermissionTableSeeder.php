d6f146798e8d7ce0478a6c13f1ea55530a4dab86
zjien <694396727@qq.com>
2016-11-04 14:49:50 +0800

    添加PermissionTableSeeder

---
+++ database/seeds/PermissionTableSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use tecai\Models\System\Permission;
+
+class PermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $actionMapVerb = [
+            'get-all' => 'GET',
+            'get-one' => 'GET',
+            'create' => 'POST',
+            'update' => 'PUT',
+            'delete' => 'DELETE'
+        ];
+        $resource = [
+            'admins',
+            'permissions',
+            'roles',
+            'clients',
+            'corporations',
+            'jobs',
+            'users',
+            'tags'
+        ];
+
+        $records = [];
+
+        array_map(function($r) use ($actionMapVerb, &$records) {
+            foreach($actionMapVerb as $a => $v) {
+                $name = $a . '-' . $r;
+                $verb = $v;
+
+                $r = $a == 'get-one' ? $r . '/{id}' : $r;
+                $uri = '/' . $r;
+
+                $type = Permission::TYPE_PUBLIC;
+                $status = Permission::STATUS_OPEN;
+                $display_name = ucwords($name);
+                $description = 'This is ' . $name . 'permission';
+                $updated_at = $created_at = date('Y-m-d H:i:s');
+                $records[] = compact('name', 'verb', 'uri', 'type', 'status', 'display_name', 'description', 'created_at', 'updated_at');
+            }
+        }, $resource);
+
+        Permission::insert($records);
+    }
+}
