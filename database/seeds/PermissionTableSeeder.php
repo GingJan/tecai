498fce40547c41a371354df2fc836fcb0f4a0d1d
zjien <694396727@qq.com>
2016-11-04 15:21:02 +0800

    小修改

--- database/seeds/PermissionTableSeeder.php
+++ database/seeds/PermissionTableSeeder.php
@@ -43,7 +43,7 @@ class PermissionTableSeeder extends Seeder
                 $type = Permission::TYPE_PUBLIC;
                 $status = Permission::STATUS_OPEN;
                 $display_name = ucwords($name);
-                $description = 'This is ' . $name . 'permission';
+                $description = 'This is ' . $name . ' permission';
                 $updated_at = $created_at = date('Y-m-d H:i:s');
                 $records[] = compact('name', 'verb', 'uri', 'type', 'status', 'display_name', 'description', 'created_at', 'updated_at');
             }
