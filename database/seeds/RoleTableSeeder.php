9934779cee4df3354ffc4e539fa1ae02fc2efed0
zjien <694396727@qq.com>
2016-11-04 14:50:19 +0800

    添加RoleTableSeeder并且将Perms和Roles注册如DatabaseSeeder里

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use tecai\Models\System\Permission;
 
 class RoleTableSeeder extends Seeder
 {
@@ -11,6 +12,37 @@ class RoleTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(\tecai\Models\System\Role::class, 2)->create();
+        $clients = Permission::where('name', 'like', '%clients%')->get()->toArray();
+        $corporations = Permission::where('name', 'like', '%corporations%')->get()->toArray();
+        $jobs = Permission::where('name', 'like', '%jobs%')->get()->toArray();
+        $users = Permission::where('name', 'like', '%users%')->get()->toArray();
+        $tags = Permission::where('name', 'like', '%tags%')->get()->toArray();
+
+        factory(\tecai\Models\System\Role::class, 'root')->create();
+
+        $roleAdmin = factory(\tecai\Models\System\Role::class, 'admin')->create();
+        $this->attachPermission($roleAdmin, $clients, $corporations, $jobs, $users, $tags);
+
+        $roleLegaler = factory(\tecai\Models\System\Role::class, 'legaler')->create();
+        $this->attachPermission($roleLegaler, $corporations, $jobs, $users, $tags);
+
+
+    }
+
+    /**
+     * @param \tecai\Models\System\Role $role
+     * @param Permission $perms
+     */
+    protected function attachPermission($role) {
+        $perms = func_get_args();
+        array_shift($perms);
+//        dd($perms);
+        foreach ($perms as $perm) {
+            $method = 'attachPermission';
+            $method = is_array($perm) ? $method . 's' : $method;
+            $role->$method($perm);
+        }
+
+        $role->save();
     }
 }
