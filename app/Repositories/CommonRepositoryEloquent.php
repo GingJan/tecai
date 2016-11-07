9f0a8181f4090da88c87379fe8cf58ce3bb2789b
zjien <694396727@qq.com>
2016-11-07 15:41:21 +0800

    修改CommonRepository的findOneByField和deleteByField

--- app/Repositories/CommonRepositoryEloquent.php
+++ app/Repositories/CommonRepositoryEloquent.php
@@ -4,10 +4,12 @@ namespace tecai\Repositories;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Prettus\Repository\Eloquent\BaseRepository;
 use Prettus\Repository\Events\RepositoryEntityDeleted;
+use Prettus\Repository\Traits\CacheableRepository;
 use tecai\Models\User\Job;
 use tecai\Repositories\Interfaces\CommonRepository;
 
 abstract class CommonRepositoryEloquent extends BaseRepository implements CommonRepository {//这个抽象类其实没必要的，下面的方法是可以放入BaseRepository里
+    use CacheableRepository
     /**
      * 不可更新字段
      * @var array
@@ -30,47 +32,15 @@ abstract class CommonRepositoryEloquent extends BaseRepository implements Common
      * @return mixed
      */
     public function findOneByField($field, $value = null, $columns = ['*']) {
-        $model = parent::findWhere([$field => $value] ,$columns)->first();
+        $model = $this->findWhere([$field => $value] ,$columns)->first();
         if( is_null($model) ) {
             throw (new ModelNotFoundException)->setModel(get_class($this->model));
         }
         return $model;
     }
 
-
-
-    public function listByLimit($where = null) {
-//        dd($where);
-        $model = $this->model;
-        $model = New Job();
-//        $where['limit'] = !empty($where['limit']) && $where['limit'] >= 0 ? $where['page'] : config('repository.pagination.limit', 10);
-//        $where['page'] = !empty($where['page']) && $where['page'] >= 1? ($where['page']-1)*$where['limit'] : 0;
-        $where['sortedBy'] = empty($where['sortedBy']) ? 'created_at' : $where['sortedBy'];
-        $where['orderBy'] = empty($where['orderBy'])? 'DESC' : $where['orderBy'];
-        return $model->orderBy($where['sortedBy'], $where['orderBy'])
-                ->paginate(10);
-//                ->appends(['sort' => $where['sort']])
-//                ->appends(['order' => $where['order']]);
-    }
-
     public function deleteByField($field, $value) {
-
-        $this->applyScope();
-
-        $temporarySkipPresenter = $this->skipPresenter;
-        $this->skipPresenter(true);
-
-        $model = $this->model->where($field, '=', $value)->firstOrFail([$field]);//修改了这里，不一定非要通过主键id删除
-        $originalModel = clone $model;
-
-        $this->skipPresenter($temporarySkipPresenter);
-        $this->resetModel();
-
-        $deleted = $model->delete();
-
-        event(new RepositoryEntityDeleted($this, $originalModel));
-
-        return $deleted;
+        return $this->deleteWhere([$field => $value]);
     }
 
 }
