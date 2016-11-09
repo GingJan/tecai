e26185deaf6008d04d690637563424d5a78f1b5a
zjien <694396727@qq.com>
2016-11-09 21:58:41 +0800

    添加缓存相关方法

--- app/Repositories/System/PermissionRepositoryEloquent.php
+++ app/Repositories/System/PermissionRepositoryEloquent.php
@@ -2,9 +2,11 @@
 
 namespace tecai\Repositories\System;
 
+use Illuminate\Contracts\Support\Arrayable;
 use Prettus\Repository\Contracts\CriteriaInterface;
 use Prettus\Repository\Criteria\RequestCriteria;
 use Prettus\Validator\Contracts\ValidatorInterface;
+use tecai\Cache\Facades\Cache;
 use tecai\Repositories\CommonRepositoryEloquent;
 use tecai\Repositories\Interfaces\System\PermissionRepository;
 use tecai\Models\System\Permission;
@@ -82,5 +84,34 @@ class PermissionRepositoryEloquent extends CommonRepositoryEloquent implements P
         $attributes['status'] = $attributes['status'] == Permission::STATUS_CLOSE ? Permission::STATUS_CLOSE : Permission::STATUS_OPEN;
 
         return parent::update($attributes, $id);
+    }
+
+    public function setCache($cacheTimes, $perms = null)
+    {
+//        $key = $this->cacheKey . 'perms:';
+
+        if( $perms instanceof Arrayable) {
+            foreach ($perms as $perm)
+            {
+                if ($perm::STATUS_OPENING == $perm->status)//关闭的资源是否应该还要放入缓存
+                    Cache::hashs('perms:' . $perm->id)->set(['type'=>$perm->type, 'verb'=>$perm->verb, 'uri'=>$perm->uri]);
+            }
+        }
+
+    }
+
+    public function allFromCache($columns = null){
+        $result = Cache::keys($this->cacheKey . 'perms:*')->sort();
+        return empty($result) ? null : $result;
+    }
+
+    public function paginateFromCache($columns = null){
+        $result = Cache::keys($this->cacheKey . 'perms:*')->sort(false, request('page', 1), config('repository.pagination.limit'));
+        return empty($result) ? null : $result;
+    }
+
+    public function findFromCache($id, $columns = null) {
+        dd($columns);
+        return Cache::hashs('perms:' . $id)->get($columns);
     }
 }
