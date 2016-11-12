5250b72e00ece025eef311e01c7e2356c6c12cf9
zjien <694396727@qq.com>
2016-11-12 22:09:02 +0800

    添加clientRepository

---
+++ app/Repositories/Interfaces/Organization/ClientRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace tecai\Repositories\Interfaces\Organization;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+use tecai\Repositories\Interfaces\CommonRepository;
+
+/**
+ * Interface ClientRepository
+ * @package namespace tecai\Repositories\Interfaces\Organization;
+ */
+interface ClientRepository extends RepositoryInterface, CommonRepository
+{
+    //
+}
