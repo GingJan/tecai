3d11225a283d53b94f0268bcdf09f48a701249d8
zjien <694396727@qq.com>
2016-11-16 16:28:18 +0800

    添加school模型和仓库

---
+++ app/Repositories/Interfaces/User/SchoolRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace tecai\Repositories\Interfaces\User;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+use tecai\Repositories\Interfaces\CommonRepository;
+
+/**
+ * Interface SchoolRepository
+ * @package namespace tecai\Repositories\Interfaces\User;
+ */
+interface SchoolRepository extends RepositoryInterface, CommonRepository
+{
+    //
+}
