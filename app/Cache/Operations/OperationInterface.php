2d453b3ff0a0a856e493dc447200f3bfaa1647a4
zjien <694396727@qq.com>
2016-11-25 23:32:33 +0800

    修改Operation接口

--- app/Cache/Operations/OperationInterface.php
+++ app/Cache/Operations/OperationInterface.php
@@ -20,4 +20,20 @@ interface OperationInterface
      * @return mixed
      */
     public function get($values);
+
+    /**
+     * Get an item from the cache, or store the default value.
+     *
+     * @param  string $value
+     * @param  \Closure  $callback
+     * @param  \DateTime|int  $minutes
+     * @return mixed
+     */
+    public function remember($value, \Closure $callback, $minutes = null);
+
+    /**
+     * @return mixed
+     */
+    public function clean();
+
 }
